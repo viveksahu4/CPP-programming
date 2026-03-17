7ff307ee45bd71202f8e5d44c78ca2dd95c18120
Vivek sahu <vickysv1610@gmail.com>
2026-03-17 20:02:51 +0530

    first class

---
+++ OOPs/class.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Hero{ // here to create class that name is Hero
+
+    // this lines consider some properties 
+    int helath;
+
+
+
+
+
+};
+int main(){
+    // here to create obj 
+    Hero h1;
+
+    cout<<"size: "<<sizeof(h1);
+
+
+    return 0;
+}
