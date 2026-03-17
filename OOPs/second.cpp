9591a18f9b20d4d7142f361807bd49c297e5243c
Vivek sahu <vickysv1610@gmail.com>
2026-03-17 20:36:24 +0530

    .

---
+++ OOPs/second.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include "first.cpp"
+using namespace std;
+
+int main(){
+    // here to create obj 
+    Hero h1;
+
+    cout<<"size: "<<sizeof(h1);
+
+
+    return 0;
+}
