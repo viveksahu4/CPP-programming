1cad6669772ec8e52a1eb77beb763fbdc010037e
Vivek sahu <vickysv1610@gmail.com>
2026-03-21 11:40:01 +0530

    getter setter for access private modifiers

--- OOPs/Propertiesaccess.cpp
+++ OOPs/Propertiesaccess.cpp
@@ -6,23 +6,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Hero{
-    public:
+    private:
     //proeprties 
     int health;
+
+    public:
     char level;
     
     void print(){
         cout<<level<<endl;
+    }
+
+    int getHealth(){ // getter
+        return health;
+    }
+
+    char getLevel(){ //getter
+        return level;
+    }
+
+    void setHealth(int h){ //setter
+        health = h;
+    }
+
+    void setLevel(char ch){ //setter
+        level = ch;
     }
 };
 
 
 int main(){
     Hero vivek;
-    vivek.health = 80;
+    // cout<<vivek.getHealth()<<endl;
+    // cout<<vivek.getLevel()<<endl;
+
+    //vivek.health = 80;
+    vivek.setHealth(90);
     vivek.level = 'A';
 
-    cout<<vivek.health<<endl;
+    cout<<vivek.getHealth()<<endl;
     cout<<vivek.level<<endl;
     return 0 ;
 }
