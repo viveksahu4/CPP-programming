7833d31227b1a81675f913e78bd2092b013248af
Vivek sahu <vickysv1610@gmail.com>
2026-03-21 12:27:24 +0530

    constructor create and called both statically and dynamically

---
+++ OOPs/constructor.cpp
@@ -0,0 +1,54 @@
+//public means you can access inside the class as well as outside the class 
+//In private you can access only inside the class 
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Hero{
+    private:
+    //proeprties 
+    int health;
+
+    public:
+    char level;
+    //constructor 
+    Hero(){
+        cout<<"constructor called "<<endl;
+    }
+    
+    void print(){
+        cout<<level<<endl;
+    }
+
+    int getHealth(){ // getter
+        return health;
+    }
+
+    char getLevel(){ //getter
+        return level;
+    }
+
+    void setHealth(int h){ //setter
+    health = h;
+        
+    }
+
+    void setLevel(char ch){ //setter
+        level = ch;
+    }
+};
+
+
+int main(){
+    cout<<"hi"<<endl;
+    //obj create statically  
+    Hero vivek; // behind the scene jab obj create krte h jab ek cheez call hoti h jisko hmm bolte h Constructor aur ha iska koe return type nhi hota 
+    cout<<"hello"<<endl;
+
+    
+    //this is dynamically create obj.
+    Hero *h = new Hero;
+
+    return 0 ;
+}
