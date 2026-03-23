a96cd776ed4e55f245cb74887a8a55e6bf9a928b
Vivek sahu <vickysv1610@gmail.com>
2026-03-23 20:08:45 +0530

    encapsulation

---
+++ OOPs/encapsulation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+
+    private:
+    string name;
+    int age;
+    int year;
+
+    //  Student(){
+    //     this->name = name;
+    //     this->age = age;
+    //     this->year = year;
+    // }
+    public:
+    int getage(){
+        return this->age;
+    }
+
+};
+
+
+int main(){
+    Student first;
+    cout<<"all good";
+
+    return 0;
+}
